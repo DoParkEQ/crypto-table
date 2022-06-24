9c26c3cc7b2bd60242e85e09cbc490c974f44332
DoParkEQ <do.park@eqworks.com>
2022-06-23 18:56:15 -0700

    table - created a base table UI

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { getList, getPrice } from './api';
 
 const URL = 'https://api.coingecko.com/api/v3/coins/joe/history?date=30-12-2017'
 
+const formatDate = (date, option = {}) => date.toLocaleDateString('en-GB', option)
+
+const formatPrice = (price) => price.toLocaleString('en-GB', { style: 'currency', currency: 'CAD'})
+
 function App() {
 
   const [data, setData] = useState()
@@ -24,8 +28,23 @@ function App() {
     }
     fetchData()
   }, [])
-
-  return <div></div>
+  console.log(data)
+  return  <table>
+  <tr>
+    <th>Date</th>
+    <th>Day of the week</th>
+    <th>1 JOE to CAD</th>
+    <th>24hr Changes</th>
+    <th>Change %</th>
+  </tr>
+    {data.map(({ price, date, changeInPercentage, changeInAmount }) => <tr>
+      <td>{formatDate(date, { year: 'numeric', month: 'long', day: '2-digit' })}</td>
+      <td>{formatDate(date, {weekday: 'long'})}</td>
+      <td>{formatPrice(price)}</td>
+      <td>{formatPrice(changeInAmount)}</td>
+      <td>{`${changeInPercentage.toFixed(1)}%`}</td>
+    </tr>)}
+</table>
 }
 
 export default App;
