097b12ae8c01d93e1f27e2a83cb4b980ecf3b1e9
DoParkEQ <do.park@eqworks.com>
2022-06-23 19:38:19 -0700

    hooks - custom hook created

---
+++ src/hooks.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import { getList } from './api';
+export const useFetchHistory = () => {
+  const [data, setData] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getList()
+        setData(data)
+      }
+      catch (e) {
+        setIsError(true)
+      }
+      setIsLoading(false)
+
+    }
+
+    if (isLoading && !isError) {
+      fetchData()
+    }
+
+  }, [isLoading, isError])
+
+  const refetch = () => {
+    setIsLoading(true)
+    setIsError(false)
+  }
+  return {data, isLoading, isError, refetch}
+}
